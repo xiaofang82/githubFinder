41788fc6a3e6de9be4d65cda3148231851d7fc09
XiaoFang <yangxiaofang82@gmail.com>
2024-06-14 10:02:56 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './css/index.css';
 import App from './App';
-
+const basename = process.env.NODE_ENV === 'production' ? '/githubFinder' : '/';
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={basename}>
       <App />
     </Router>
   </React.StrictMode>
